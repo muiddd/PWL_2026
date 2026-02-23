fa223559c462a2b12f05e5635d975a60a005154a
muid <muidabdulmuid99@gmail.com>
2026-02-23 23:03:48 +0700

    p1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world ', function () {
+    return 'world';
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'selamat datang';
+});
+
+Route::get('/about', function () {
+    return '244107020006 Abdul Muid';
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
 });
