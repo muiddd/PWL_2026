81f12d8893ea599afc986fe5ef66f4106c0947c3
muid <muidabdulmuid99@gmail.com>
2026-02-26 21:28:46 +0700

    p2_no11

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PhotoController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
@@ -31,4 +32,12 @@ Route::get('/about', [AboutController::class, 'about']);
 
 Route::get('/articles/{id}', action: [ArticleController::class, 'articles']);
 
+Route::resource('photos', PhotoController::class);
 
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
